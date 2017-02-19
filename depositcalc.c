e784110ec3d026b808def00963507883e0f7d377
ToDDdi <kolokolnikovkm@gmail.com>
2017-02-20 01:29:18 +0700

    Check input data

--- depositcalc.c
+++ depositcalc.c
@@ -9,6 +9,11 @@ int main()
 	scanf("%d",&time);
 	printf("Sum deposit:\n");
 	scanf("%f",&deposit);
-	
+
+	if((time>365)||(time<0)||(deposit<10000)) 
+	{
+        perror("incorrect input");
+        return 0;
+    }
     return 0;
 }
