8b6459d27efb8c82598681493cbd0b642896cddf
ToDDdi <kolokolnikovkm@gmail.com>
2017-02-20 01:25:30 +0700

    Input data

--- depositcalc.c
+++ depositcalc.c
@@ -2,6 +2,13 @@
 
 int main() 
 {
-    
+    int time,i;
+	float deposit;
+
+	printf("Time deposit:\n");        
+	scanf("%d",&time);
+	printf("Sum deposit:\n");
+	scanf("%f",&deposit);
+	
     return 0;
 }
